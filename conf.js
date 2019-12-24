fe23532e450e3be2074e6ae1a2815a7b3eaec397
gxggsrmyy <gxggsrmyy@gmail.com>
2019-12-24 18:33:14 +0800

    版本日志

--- conf.js
+++ conf.js
@@ -88,8 +88,8 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
+    'https://www.pornhub2.com/': {
+      redir: 'https://www.pornhub.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
