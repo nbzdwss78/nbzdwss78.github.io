e727101ba1092a9613e0bdfb31290cb661dc684f
gxggsrmyy <gxggsrmyy@gmail.com>
2021-06-29 11:57:09 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -53,7 +53,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'cfworker',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
