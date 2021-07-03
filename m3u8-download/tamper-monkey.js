4c1f8769c4925a4dd675a1728958934aa5e917de
gxggsrmyy <gxggsrmyy@gmail.com>
2021-07-03 17:11:06 +0800

    ok

--- m3u8-download/tamper-monkey.js
+++ m3u8-download/tamper-monkey.js
@@ -47,12 +47,12 @@
     ajax({
       url,
       success: (fileStr) => {
-        if (fileStr.indexOf('.ts') > -1) {
+        if (fileStr.indexOf('.ts') > -1 || fileStr.indexOf('.image') > -1) {
           appendDom()
           m3u8Target = url
           console.log('【m3u8】----------------------------------------')
           console.log(url)
-          console.log('http://blog.luckly-mjw.cn/tool-show/m3u8-downloader/index.html?source=' + url)
+          console.log('https://gxggsrmyy.github.io/m3u8-download/index.html?source=' + url)
         }
       }
     })
@@ -135,7 +135,7 @@
     })
 
     m3u8Jump.addEventListener('click', function() {
-      window.open('http://blog.luckly-mjw.cn/tool-show/m3u8-downloader/index.html?source=' + m3u8Target)
+      window.open('https://gxggsrmyy.github.io/m3u8-download/index.html?source=' + m3u8Target)
     })
 
     m3u8Append.addEventListener('click', function() {
