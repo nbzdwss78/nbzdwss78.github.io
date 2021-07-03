28d9442d442a958b681760f2a3657c1d3aad9f59
gxggsrmyy <gxggsrmyy@gmail.com>
2021-07-03 18:17:25 +0800

    Update tamper-monkey.js

--- m3u8-downloader/tamper-monkey.js
+++ m3u8-downloader/tamper-monkey.js
@@ -130,7 +130,7 @@
     })
 
     m3u8Jump.addEventListener('click', function() {
-      window.open('http://blog.luckly-mjw.cn/tool-show/m3u8-downloader/index.html?source=' + m3u8Target)
+      window.open('https://gxggsrmyy.github.io/m3u8-downloader/index.html?source=' + m3u8Target)
     })
 
     m3u8Append.addEventListener('click', function() {
