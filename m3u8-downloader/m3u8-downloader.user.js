a7b8d8f41fcbc58930b74ffbae2bc56c70da0570
gxggsrmyy <gxggsrmyy@gmail.com>
2022-10-10 15:55:16 +0800

    Update m3u8-downloader.user.js

--- m3u8-downloader/m3u8-downloader.user.js
+++ m3u8-downloader/m3u8-downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         m3u8-downloader
 // @namespace    https://github.com/Momo707577045/m3u8-downloader
-// @version      0.6.0
+// @version      0.8.0
 // @description  https://github.com/Momo707577045/m3u8-downloader 配套插件
 // @author       Momo707577045
 // @include      *
