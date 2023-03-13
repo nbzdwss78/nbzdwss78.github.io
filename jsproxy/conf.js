380131668a4f059b31d915c63efa5dd5a2e9ff0f
gxggsrmyy <gxggsrmyy@gmail.com>
2023-03-13 17:18:32 +0800

    ok

--- jsproxy/conf.js
+++ jsproxy/conf.js
@@ -6,7 +6,7 @@ jsproxy_config({
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
-    enable: false,
+    enable: true,
     ver: 62
   },
 
@@ -23,30 +23,29 @@ jsproxy_config({
     'demo-sg': {
       label: '演示服务-新加坡节点',
       lines: {
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
     'mysite': {
       label: '当前站点',
       lines: {
         [location.host]: 1,
-        //'https://www.nbzdwss78.tk': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: 'cfworker',
-      hidden: false,
+      label: '',
+      hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 1,
+        //'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         //'b.007.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
-        
+        //'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -60,13 +59,13 @@ jsproxy_config({
   /**
    * 加速节点
    */
-  node_acc: 'mysite',
+  node_acc: 'cfworker',
 
   /**
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  //assets_cdn: 'https://cdn.jsdelivr.net/gh/gxggsrmyy/gxggsrmyy.github.io@master/jsproxy/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
@@ -75,7 +74,7 @@ jsproxy_config({
   index_path: 'index_v3.html',
 
   // 支持 CORS 的站点列表（实验中...）
-  //direct_host_list: 'cors_v1.txt',
+  direct_host_list: 'cors_v1.txt',
 
   /**
    * 自定义注入页面的 HTML
@@ -89,8 +88,8 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub2.com/': {
-      redir: 'https://www.pornhub.com/'
+    'https://www.pornhub.com/': {
+      redir: 'https://php.net/'
     },
     'http://haha.com/': {
       content: 'Hello World'
